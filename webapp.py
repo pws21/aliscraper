fe8a906671a6a64f443a0c04efadf8a0741871ae
Pavel Shiryaev <pws@front.ru>
2016-09-15 14:21:07 +0300

    service unavailable response

--- webapp.py
+++ webapp.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 def update_db():
     url = request.args.get('url')
     try:
-        scraper = AliProductScraper(url)
-        rows = scraper.get_variants()
-        write_to_db(rows)
-        return json.dumps({"result":"OK"})
+        save_variants(url, write_to_db)
+        return json.dumps({"result": "OK"})
     except NotProductPage:
         abort(422)
+    except ServiceUnavailable:
+        abort(503)
 
 @app.route("/sample")
 def sample():
@@ -23,6 +23,8 @@ def sample():
         return json.dumps(rows, ensure_ascii=False)
     except NotProductPage:
         abort(422)
+    except ServiceUnavailable:
+        abort(503)
 
 if __name__ == "__main__":
     app.run()
